2e4bb94d871b759344c39a798fe2885bd8eb7a7c
thana12604 <54341696+thana12604@users.noreply.github.com>
2020-09-30 10:11:42 +0700

    Add files via upload

---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="example-1">
+  <button @click="upFunction" class="btn btn-info">{{lable}} </button>
+  <p v-bind:style="{ fontsize:  fsize = 'px'}">HELLO {{counter}}</p>
+</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            counter: 0,
+            fsize: 50,
+        }
+  },
+  props: {
+    lable: String,
+},
+    methods:{
+        upFunction: function(){
+            this.counter += 1
+            this.fsize +=10
+
+            console.log('counter ='+this.counter);
+            console.log('fsize ='+this.fsize);
+        },
+    },
+
+
+}
+
+
+</script>
+
+<style>
+
+</style>
